4eddfcfda07288c37985fcf0422e2de91bd8a5de
Yuumei <shjesna@protonmail.com>
2026-07-14 21:10:18 +0200

    Player movement not rotates around the board in a circle

--- main.asm
+++ main.asm
@@ -8,6 +8,11 @@ section .text
 extern _exit
 extern printf
 
+;; Libm
+extern cosf
+extern sinf
+extern atan2f
+
 ;; Raylib
 extern InitWindow
 extern CloseWindow
@@ -44,6 +49,7 @@ main:
    call ClearBackground
 
    call handle_input
+   call reposition_player
    call draw_player
 
    call EndDrawing
@@ -62,9 +68,9 @@ draw_player:
    ;; Rectangle
    pxor xmm0, xmm0
    pxor xmm2, xmm2
-   mov  rdx,  [player]
-   movq xmm1, [player+8] ;; width and height
-   movq xmm0, rdx        ;; x and y
+   mov  rdx,  [player.x] ;; Includes y
+   movq xmm1, [player.width] ;; Includes height
+   movq xmm0, rdx
 
    ;; Origin
    movq xmm2, [PADDLE_ORIGIN]
@@ -100,15 +106,15 @@ handle_input:
 .KEY_A_COND:
    mov rdi, 65 ;; KEY_A
    call IsKeyDown
-   test rax, 65
+   test rax, 65 ;; DO NOT TOUCH
    jz .KEY_A_END
 .KEY_A_BODY:
    call GetFrameTime
    movss xmm1, [PADDLE_SPEED]
    mulss xmm1, xmm0
-   movss xmm0, [player.x]
+   movss xmm0, [player.z]
    subss xmm0, xmm1
-   movd [player.x], xmm0
+   movd [player.z], xmm0
 .KEY_A_END:
 .KEY_D_COND:
    mov rdi, 68 ;; KEY_D
@@ -119,10 +125,107 @@ handle_input:
    call GetFrameTime
    movss xmm1, [PADDLE_SPEED]
    mulss xmm1, xmm0
-   movss xmm0, [player.x]
+   movss xmm0, [player.z]
    addss xmm0, xmm1
-   movd [player.x], xmm0
+   movd [player.z], xmm0
 .KEY_D_END:
+   mov rsp, rbp
+   pop rbp
+   ret
+
+reposition_player:
+   ;; @Todo: Some registers are callee saved and others are caller saved.
+   ;;        Look up which registers you need to save and which ones you can discard
+   ;;        and modify the assembly accordingly
+
+   push rbp
+   mov rbp, rsp
+
+   mov eax, __float32__(400.0) ;; Center x
+   mov ebx, __float32__(300.0) ;; Center y
+   mov ecx, __float32__(350.0) ;; Distance
+
+   ;; angle_rad = rotation * (PI / 180.0)
+   mov esi, __float32__(180.0)
+   movd xmm0, [PI]
+   movd xmm1, esi
+   divss xmm0, xmm1
+   movd xmm1, [player.z]
+   mulss xmm1, xmm0
+   movd esi, xmm1
+   push rsi
+
+   ;; x = cx + distance * cos(angle_rad)
+   ;; cos(angle_rad)
+   push rax
+   push rbx
+   push rcx
+   movss xmm0, xmm1
+   call cosf
+   pop rcx
+   pop rbx
+   pop rax
+   ;; distance * cos(angle_rad)
+   movd xmm1, ecx
+   mulss xmm1, xmm0
+   ;; cx + distance
+   movd xmm0, eax
+   addss xmm0, xmm1
+   movd esi, xmm0
+   pop r8 ;; angle_rad
+   push rsi
+
+   ;; y = cy + distance * sin(angle_rad)
+   ;; sin(angle_rad)
+   push rax
+   push rbx
+   push rcx
+   movd xmm0, r8d
+   call sinf
+   pop rcx
+   pop rbx
+   pop rax
+   ;; distance * sin(angle_rad)
+   movd xmm1, ecx
+   mulss xmm1, xmm0
+   ;; cy + distance
+   movd xmm0, ebx
+   addss xmm0, xmm1
+   movd r9d, xmm0 ;; y
+   pop r8         ;; x
+
+   movd xmm1, r8d
+   movd [player.x], xmm1
+   movd [player.y], xmm0
+
+   ;; dy = cy - y
+   movd xmm0, ebx
+   movd xmm1, r9d
+   subss xmm0, xmm1
+   movd r9d, xmm0
+
+   ;; dx = cx - x
+   movd xmm0, eax
+   movd xmm1, r8d
+   subss xmm0, xmm1
+   movss xmm1, xmm0
+
+   ;; rad = atan2f(dy, dx)
+   movd xmm0, r9d
+   call atan2f
+   movss xmm2, xmm0
+
+   ;; rad * (180.0 / pi)
+   mov eax, __float32__(180.0)
+   movd xmm0, eax
+   movd xmm1, [PI]
+   divss xmm0, xmm1
+   mulss xmm2, xmm0
+   mov eax, __float32__(90.0) ;; offset
+   movd xmm0, eax
+   addss xmm2, xmm0
+   movd [player.rotation], xmm2
+
    mov rsp, rbp
    pop rbp
    ret
@@ -135,20 +238,22 @@ WINDOW:
    align 8
 
 player:
-   player.x:        dd 50.0
-   player.y:        dd 50.0
-   player.width:    dd 125.0
-   player.height:   dd 30.0
+   player.x:        dd 400.0
+   player.y:        dd 300.0
+   player.z:        dd 0.0
+   player.width:    dd 150.0
+   player.height:   dd 25.0
    player.rotation: dd 0.0
-   align 4
 
 PADDLE_ORIGIN:
-   dd 1.0 ;; x
-   dd 1.0 ;; y
+   dd 75.0 ;; x
+   dd 150.0 ;; y
    align 4
 
 PADDLE_COLOR: dd 0xffffffff
 PADDLE_SPEED: dd 350.0
+
+PI: dd 3.14159265358979323846
 
 debug_i32_fmt: db "%d", 10, 0
 
