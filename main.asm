07649f44fac22c9ed1095dcd70e501f669c6dc57
Yuumei <shjesna@protonmail.com>
2026-07-14 15:49:16 +0200

    linked with raylib

--- main.asm
+++ main.asm
@@ -1,17 +1,55 @@
 global _start
 section .text
 
-extern printf
+;; Libc
 extern _exit
 
-_start:
-   mov rdi, msg
-   call printf
+;; Raylib
+extern InitWindow
+extern CloseWindow
+extern WindowShouldClose
+extern BeginDrawing
+extern EndDrawing
+extern ClearBackground
 
+_start:
+   xor rbp, rbp
+   and rsp, -16
+   sub rsp, 8
+
+   call main
    xor rdi, rdi
    call _exit
 
+main:
+   push rbp
+   mov rbp, rsp
+   sub rsp, 40
+
+   mov rdi, 800
+   mov rsi, 600
+   mov rdx, WIN_TITLE
+   call InitWindow
+
+.window_loop_cond:
+   call WindowShouldClose
+   test rax, rax
+   jnz .window_loop_end
+.window_loop_body:
+   call BeginDrawing
+   mov rdi, 0xFF181818
+   call ClearBackground
+   call EndDrawing
+   jmp .window_loop_cond
+.window_loop_end:
+
+   call CloseWindow
+   mov rsp, rbp
+   pop rbp
+   ret
+
 section .data
-msg: db "Zhyivannye miratte!", 10, 0
-msg_len: equ ($ - msg) - 1
+WIN_WIDTH: dd 800
+WIN_HEIGHT: dd 600
+WIN_TITLE: db "Pasm", 0
 
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 .ONESHELL:
 
 pasm: main.o
-	ld main.o -o pasm -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lc
+	ld main.o -o pasm -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lc -Lvendor/raylib/src -lraylib -lm -lX11
 	rm main.o
 
 main.o: main.asm
